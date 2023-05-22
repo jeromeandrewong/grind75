2c94baa0927c92a34506801b60dc9d91946096ae
Jerome Wong <jeromewongjh@gmail.com>
2023-05-22 18:49:07 +0800

    feat: inverse binary tree

---
+++ invert-binary-tree.ts
@@ -0,0 +1,22 @@
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+function invertTree(root: TreeNode | null): TreeNode | null {
+  if (root === null) return null;
+  // swapping left and right sub trees
+  let temp = root.left;
+  root.left = root.right;
+  root.right = temp;
+  // traverse downwards till leaves
+  invertTree(root.left);
+  invertTree(root.right);
+
+  return root;
+}
