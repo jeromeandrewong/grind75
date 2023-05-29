4cfaa093f4252fe422c732b538b661dae9e6694f
Jerome Wong <jeromewongjh@gmail.com>
2023-05-29 16:13:46 +0800

    feat: climbing stairs

---
+++ climbing-stairs.py
@@ -0,0 +1,10 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        def climb(n):
+            if n in memo:
+                return memo[n]
+            else:
+                memo[n] = climb(n-1) + climb(n-2)
+                return memo[n]
+        memo = {1:1, 2:2}
+        return climb(n)
